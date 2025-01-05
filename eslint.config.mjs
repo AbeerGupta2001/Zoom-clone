cc84c06f19451fdc920a3b62b3941abbaedf320b
Abeer <abeer@localhost.localdomain>
2025-01-05 18:54:46 +0530

    First Commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  "@typescript-eslint/ban-ts-comment"=false
 ];
 
 export default eslintConfig;
